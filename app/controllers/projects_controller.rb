7322cd0a83926114945c621f7eac6e06f68dad3b
tjhaines-cap <tjhaines@eckerd.edu>
2022-05-23 17:24:53 -0600

    Feat: create contestant projects controller and create method in it

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,8 +4,5 @@ class ProjectsController < ApplicationController
         @project = Project.find(params[:project_id])
     end
 
-    def update
-        ContestantProject.create(contestant_id: params[:contestant_id], project_id: params[:project_id])
-        redirect_to "/projects/#{params[:project_id]}"
-    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/projects/:project_id', to: 'projects#show'
-  patch '/projects/:project_id', to: 'projects#update'
+  # patch '/projects/:project_id', to: 'projects#update'
+  post '/projects/:project_id/contestants', to: 'contestant_projects#create'
 
   get '/contestants', to: 'contestants#index'
 end
